a2276a7e6fad7398e08215003b047afe0bc0c888
Neil Song <song.neil.song@gmail.com>
2021-04-01 10:31:53 -0500

    misc changes

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ read -p 'Cloud Run Service [redact-run]: ' service
 service=${service:-redact-run}
 
 # Build and Deploy Cloud Run service
-gcloud builds submit --tag gcr.io/${project_id}/${image_tag} --config=cloudbuild.yaml --substitutions=_SERVICE_NAME="${service}",_REDACTED_BUCKET_NAME="${redact_bucket}"
-service_url=$(gcloud run deploy ${service} --image gcr.io/${project_id}/${image_tag} --format='value(status.url)' --platform managed)
+gcloud builds submit --config=cloudbuild.yaml --substitutions=_IMAGE_TAG="${image_tag}",_REDACTED_BUCKET_NAME="${redact_bucket}"
+gcloud run deploy ${service} --image us.gcr.io/${project_id}/${image_tag} --platform=managed --no-allow-unauthenticated
